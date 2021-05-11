8921ffd3b1dda0d031a7db56512f15cd298049e9
KaranWaghere <karanwaghere11@gmail.com>
2021-05-11 18:04:53 +0200

    Updated new rooms and customers

--- InsertDataMongoDB.js
+++ InsertDataMongoDB.js
@@ -13,12 +13,30 @@ db.customers.insertMany([
     name: "Bob",
     address: "xyz",
   },
+  {
+    name: "Markus",
+    address: "a1b2c3",
+  },
+  {
+    name: "Julia",
+    address: "123",
+  },
+  {
+    name: "Jinny",
+    address: "456",
+  },
 ]);
 
 var customer1 = db.customers.findOne({ name: "John" });
 var customerId1 = customer1._id;
 var customer2 = db.customers.findOne({ name: "Bob" });
 var customerId2 = customer2._id;
+var customer3 = db.customers.findOne({ name: "Markus" });
+var customerId3 = customer3._id;
+var customer4 = db.customers.findOne({ name: "Julia" });
+var customerId4 = customer4._id;
+var customer5 = db.customers.findOne({ name: "Jinny" });
+var customerId5 = customer5._id;
 
 db.createCollection("rooms");
 db.rooms.insertMany([
@@ -33,14 +51,14 @@ db.rooms.insertMany([
       {
         startDate: "20-05-2021",
         endDate: "25-05-2021",
-        customerId: customerId1
+        customerId: customerId1,
       },
       {
         startDate: "27-05-2021",
         endDate: "02-06-2021",
-        customerId: customerId2
-      }
-    ]
+        customerId: customerId2,
+      },
+    ],
   },
   {
     number: 102,
@@ -53,9 +71,9 @@ db.rooms.insertMany([
       {
         startDate: "22-05-2021",
         endDate: "27-05-2021",
-        customerId: customerId1
-      }
-    ]
+        customerId: customerId1,
+      },
+    ],
   },
   {
     number: 103,
@@ -64,13 +82,70 @@ db.rooms.insertMany([
     features: ["Ocean View"],
     occupant: "",
     status: "vacant",
+    bookings: [{}],
+  },
+  {
+    number: 104,
+    price: 1000,
+    category: "Delux Plus",
+    features: ["Ocean View"],
+    occupant: customerId3,
+    status: "occupied",
     bookings: [
       {
-        startDate: "22-05-2021",
-        endDate: "27-05-2021",
-        customerId: customerId1
-      }
-    ]
+        startDate: "25-05-2021",
+        endDate: "29-05-2021",
+        customerId: customerId3,
+      },
+    ],
+  },
+  {
+    number: 105,
+    price: 800,
+    category: "Delux",
+    features: ["Ocean View"],
+    occupant: "",
+    status: "vacant",
+    bookings: [{}],
+  },
+  {
+    number: 106,
+    price: 1500,
+    category: "Super Delux",
+    features: ["Ocean View", "City View"],
+    occupant: customerId4,
+    status: "occupied",
+    bookings: [
+      {
+        startDate: "01-06-2021",
+        endDate: "05-06-2021",
+        customerId: customerId4,
+      },
+    ],
+  },
+  {
+    number: 107,
+    price: 600,
+    category: "Premium",
+    features: ["Ocean View"],
+    occupant: "",
+    status: "vacant",
+    bookings: [{}],
+  },
+  {
+    number: 108,
+    price: 600,
+    category: "Premium",
+    features: ["City View"],
+    occupant: customerId5,
+    status: "occupied",
+    bookings: [
+      {
+        startDate: "28-05-2021",
+        endDate: "30-05-2021",
+        customerId: customerId5,
+      },
+    ],
   },
 ]);
 
@@ -78,6 +153,18 @@ var room1 = db.rooms.findOne({ number: 101 });
 var roomId1 = room1._id;
 var room2 = db.rooms.findOne({ number: 102 });
 var roomId2 = room2._id;
+var room3 = db.rooms.findOne({ number: 103 });
+var roomId3 = room3._id;
+var room4 = db.rooms.findOne({ number: 104 });
+var roomId4 = room4._id;
+var room5 = db.rooms.findOne({ number: 105 });
+var roomId5 = room5._id;
+var room6 = db.rooms.findOne({ number: 106 });
+var roomId6 = room6._id;
+var room7 = db.rooms.findOne({ number: 107 });
+var roomId7 = room7._id;
+var room8 = db.rooms.findOne({ number: 108 });
+var roomId8 = room8._id;
 
 db.createCollection("services");
 db.services.insertMany([
@@ -133,54 +220,51 @@ var staffId3 = staff3._id;
 var staff4 = db.staff.findOne({ name: "Kim" });
 var staffId4 = staff4._id;
 
-
 db.createCollection("tasks");
 db.tasks.insertMany([
   {
     type: "luggage",
     staff: staffId1,
-    customer: customerId1,   
+    customer: customerId1,
     status: "completed",
-    feedback: "abc"
+    feedback: "abc",
   },
   {
     type: "room service",
     staff: staffId2,
-    customer: customerId2,  
+    customer: customerId2,
     status: "pending",
-    feedback: ""
-  }
+    feedback: "",
+  },
 ]);
-
 
 db.createCollection("menu");
 db.menu.insertMany([
-    {
-        name: "food1",
-        price: "50",
-    },
-    {
-        name: "food2",
-        price: "65",
-    },
-    {
-        name: "drink1",
-        price: "15",
-    },
-    {
-        name: "drink2",
-        price: "20",
-    },
-    {
-        name: "dessert1",
-        price: "15",
-    },
-    {
-        name: "dessert2",
-        price: "10",
-    }
-]
-);
+  {
+    name: "food1",
+    price: "50",
+  },
+  {
+    name: "food2",
+    price: "65",
+  },
+  {
+    name: "drink1",
+    price: "15",
+  },
+  {
+    name: "drink2",
+    price: "20",
+  },
+  {
+    name: "dessert1",
+    price: "15",
+  },
+  {
+    name: "dessert2",
+    price: "10",
+  },
+]);
 
 var food1 = db.menu.findOne({ name: "food1" });
 var foodId1 = food1._id;
@@ -256,10 +340,10 @@ db.activities.insertMany([
     reviews: ["review1", "review2"],
     bookings: {
       date: "14-06-2021",
-      time:"15:00-18:00",
-      maxSize:10,
-      bookings: [customerId2]
-    }
+      time: "15:00-18:00",
+      maxSize: 10,
+      bookings: [customerId2],
+    },
   },
   {
     name: "pottery",
@@ -270,11 +354,10 @@ db.activities.insertMany([
     reviews: ["review1", "review2"],
     bookings: {
       date: "15-06-2021",
-      time:"09:00-12:00",
-      maxSize:5,
-      bookings: [customerId2]
-    }
-
+      time: "09:00-12:00",
+      maxSize: 5,
+      bookings: [customerId2],
+    },
   },
 ]);
 
@@ -288,11 +371,10 @@ db.regroupLocations.insertMany([
   {
     name: "Hallowed Church",
     activity: [activitiesId1],
-    location:
-    {
+    location: {
       lat: 15.2222,
-      lng: 50.3333
-    }
+      lng: 50.3333,
+    },
   },
 ]);
 
@@ -303,49 +385,45 @@ db.createCollection("currentLocation");
 db.currentLocation.insertMany([
   {
     person: customerId1,
-    location:
-    {
+    location: {
       lat: -72.7738706,
-      lng: 41.6332836
-    }
+      lng: 41.6332836,
+    },
   },
   {
     person: staffId4,
-    location:
-    {
+    location: {
       lat: 15.2222,
-      lng: 50.3333
-    }
+      lng: 50.3333,
+    },
   },
 ]);
 
 db.createCollection("interests");
 db.interests.insertMany([
   {
-    name: "shopping"
+    name: "shopping",
   },
   {
-    name: "photography"
-  }
+    name: "photography",
+  },
 ]);
 
 db.createCollection("placesOfInterest");
 db.placesOfInterest.insertMany([
   {
     name: "place1",
-    location:
-    {
+    location: {
       lat: -72.7738706,
-      lng: 41.6332836
-    }
+      lng: 41.6332836,
+    },
   },
   {
     name: "place2",
-    location:
-    {
+    location: {
       lat: 15.2222,
-      lng: 50.3333
-    }
+      lng: 50.3333,
+    },
   },
 ]);
 
@@ -354,27 +432,36 @@ db.accessControl.insertMany([
   {
     room: roomId1,
     QRCode: "path/to/image",
-    pin: 1234
+    pin: 1234,
   },
   {
     room: roomId2,
     QRCode: "path/to/image",
-    pin:5678
-  }
+    pin: 5678,
+  },
+  {
+    room: roomId4,
+    QRCode: "path/to/image",
+    pin: 9123,
+  },
+  {
+    room: roomId6,
+    QRCode: "path/to/image",
+    pin: 1209,
+  },
+  {
+    room: roomId8,
+    QRCode: "path/to/image",
+    pin: 1208,
+  },
 ]);
 
 db.createCollection("roomChangeReasons");
-db.roomChangeReasons.insertOne(
-  {
-    reasons: ["reason1", "reason2", "reason3"]
-  }
-);
+db.roomChangeReasons.insertOne({
+  reasons: ["Not as per expectations", "Want a city view"],
+});
 
 db.createCollection("roomsForCleaning");
-db.roomsForCleaning.insertOne(
-  {
-    rooms: [roomId1, roomId2]
-  }
-);
-
-
+db.roomsForCleaning.insertOne({
+  rooms: [roomId1, roomId2],
+});
